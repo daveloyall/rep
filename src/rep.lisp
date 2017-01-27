07b895cb8944c65f8897cef35390298679738be5
Dave Loyall <dave@the-good-guys.net>
2017-01-27 10:18:45 -0600

    WIP. lisp-executable isn't picking up the log4slime stuff.

--- src/rep.lisp
+++ src/rep.lisp
@@ -1,15 +1,15 @@
-(defpackage "THE-GOOD-GUYS.CL-REP"
+(defpackage "THE-GOOD-GUYS.REP"
   (:use "COMMON-LISP"
         "LISP-EXECUTABLE"))
-(in-package "THE-GOOD-GUYS.CL-REP")
+(in-package "THE-GOOD-GUYS.REP")
 
 
 					;We could take arguents like this (&options help)
 					;cause lisp-executable handles command line args
 ;It's like a REPL with no Loop.
-(define-program cl-rep (&options eval &others sexp)
+(define-program rep (&options eval &others sexp)
   (ql:quickload :log4slime :silent t)
-  (log:config (log::logger '(REP)) :own :daily "log_cl-rep.txt" :trace :nopretty :noadditive)
+  (log:config (log::logger '(REP)) :own :daily "log_rep.txt" :trace :nopretty :noadditive)
   (cond (eval
 	 (let ((the-input (read-from-string (apply 'values sexp))))
 	   (log:trace the-input)
